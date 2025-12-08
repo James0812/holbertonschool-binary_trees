66a86ad81c89e9e5e2969fb0cbc2d213ca5d7731
James0812 <james.roussel008@gmail.com>
2025-12-08 12:54:05 +0100

    add the files with their code

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -22,7 +22,6 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	new_node->n = value;
 	new_node->parent = parent;
 
-	/* If parent already has a left child, make it the left child of the new node */
 	if (parent->left != NULL)
 	{
 		new_node->left = parent->left;
