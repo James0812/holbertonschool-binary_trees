933cd023eb3346aac3390f235aff444ae21890a6
James0812 <james.roussel008@gmail.com>
2025-12-09 11:42:49 +0100

    add the files with their code

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -4,7 +4,8 @@
  * binary_tree_uncle - Finds the uncle of a node
  * @node: Pointer to the node to find the uncle of
  *
- * Return: Pointer to the uncle node, or NULL if no uncle or node/parent is NULL
+ * Return: Pointer to the uncle node, or NULL if no uncle exists
+ *         or if node/parent is NULL
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
