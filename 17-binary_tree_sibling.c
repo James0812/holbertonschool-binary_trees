1129e2a451ab28c1136c700d98473f944376cfdb
James0812 <james.roussel008@gmail.com>
2025-12-09 10:45:23 +0100

    add the files with their code

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -4,10 +4,10 @@
  * binary_tree_sibling - Finds the sibling of a node
  * @node: Pointer to the node to find the sibling of
  *
- * Return: Pointer to the sibling node, or NULL if no sibling or node/parent is NULL
+ * Return: Pointer to the sibling node, or NULL if no sibling exists
+ *         or node/parent is NULL
  */
-binary_tree_t *binary_tree_sibling(
-	binary_tree_t *node)
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if (!node || !node->parent)
 		return (NULL);
