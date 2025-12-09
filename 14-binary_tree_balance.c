a8f1a14a56ce525799acc91804dae54cbf738e60
James0812 <james.roussel008@gmail.com>
2025-12-09 10:15:44 +0100

    add the files with their code

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,15 +8,15 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left_height;
-	int right_height;
+    int left_height;
+    int right_height;
 
-	if (tree == NULL)
-		return (0);
+    if (tree == NULL)
+        return (0);
 
-	left_height = tree->left ? 1 + binary_tree_balance(tree->left) : 0;
-	right_height = tree->right ? 1 + binary_tree_balance(tree->right) : 0;
+    left_height = (int)binary_tree_height(tree->left);
+    right_height = (int)binary_tree_height(tree->right);
 
-	return (left_height - right_height);
+    return left_height - right_height;
 }
 
